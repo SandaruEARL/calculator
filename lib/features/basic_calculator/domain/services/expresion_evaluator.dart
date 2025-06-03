80564ddac452ecf30203c93c05b591a087b8d69a
Sandaru Lashan Herman <165351637+SandaruEARL@users.noreply.github.com>
2025-06-03 22:21:27 +0530

    fixed_evaluator

--- lib/features/basic_calculator/domain/services/expresion_evaluator.dart
+++ lib/features/basic_calculator/domain/services/expresion_evaluator.dart
@@ -1,20 +1,25 @@
 import 'package:math_expressions/math_expressions.dart';
+import 'dart:math' as math;
 
 class ExpressionEvaluator {
-  // Simple expression evaluator (percentage conversion now handled by ViewModel)
-  double _evaluateExpression(String expression) {
+  // Simple expression evaluator with pow function support
+  static double _evaluateExpression(String expression) {
     expression = expression.replaceAll('×', '*').replaceAll('÷', '/');
-    // No percentage preprocessing needed here anymore
 
     final parser = Parser();
+
+    print(expression);
+
     final exp = parser.parse(expression);
+
     final contextModel = ContextModel();
     final eval = exp.evaluate(EvaluationType.REAL, contextModel);
+    print(eval);
 
     return eval;
   }
 
-  // this public method to call _evaluateExpression
+  // Public method to call _evaluateExpression
   double evaluate(String expression) {
     return _evaluateExpression(expression);
   }
